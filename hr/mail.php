e83c5d0df55f3928821dce7c7c620710b50f09bf
Destriy <ardar2700@gmail.com>
2017-12-13 00:40:47 +0300

    first commit

---
+++ hr/mail.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>Обратная связь</title>
+</head>
+<body>
+<?php
+
+$sendto   = "artem.sheremeta@mail.ru"; // почта, на которую будет приходить письмо
+$username = $_POST['name'];   // сохраняем в переменную данные полученные из поля c именем
+$usermail = $_POST['email']; // сохраняем в переменную данные полученные из поля c адресом электронной почты
+
+// Формирование заголовка письма
+$subject  = "Новое сообщение";
+$headers  = "From: " . strip_tags($usermail) . "\r\n";
+$headers .= "Reply-To: ". strip_tags($usermail) . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+
+// Формирование тела письма
+$msg  = "<html><body style='font-family:Arial,sans-serif;'>";
+$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Cообщение с сайта</h2>\r\n";
+$msg .= "<p><strong>От кого:</strong> ".$username."</p>\r\n";
+$msg .= "<p><strong>Почта:</strong> ".$usermail."</p>\r\n";
+$msg .= "<p><strong>Сайт:</strong> ".$usertel."</p>\r\n";
+$msg .= "</body></html>";
+
+// отправка сообщения
+if(@mail($sendto, $subject, $msg, $headers)) {
+    echo "<center><img src='images/spasibo.png'></center>";
+} else {
+    echo "<center><img src='images/ne-tpravleno.png'></center>";
+}
+
+?>
+</body>
+</html>
